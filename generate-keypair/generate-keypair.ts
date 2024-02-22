587488d2749b4ebd24d049d81f56200292e2b608
Abhiraj Aditya <abhiaditya860@gmail.com>
2024-02-23 00:16:17 +0530

    new

--- generate-keypair/generate-keypair.ts
+++ generate-keypair/generate-keypair.ts
@@ -4,6 +4,5 @@ dotenv.config();
 import { getKeypairFromEnvironment } from "@solana-developers/helpers";
 
 const keypair = getKeypairFromEnvironment("SECRET_KEY");
-console.log(keypair.publicKey.toBase58);
-console.log(keypair.secretKey);
+
 console.log("Finished");
