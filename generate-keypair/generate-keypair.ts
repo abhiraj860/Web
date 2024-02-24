c26bcedd2e5d2a61a3ee0c9f493b242a1fa86998
Abhiraj Aditya <abhiaditya860@gmail.com>
2024-02-24 11:56:17 +0530

    new-public-key-addded

--- generate-keypair/generate-keypair.ts
+++ generate-keypair/generate-keypair.ts
@@ -4,6 +4,9 @@ dotenv.config();
 import { getKeypairFromEnvironment } from "@solana-developers/helpers";
 
 const keypair = getKeypairFromEnvironment("SECRET_KEY");
-console.log("Public key is " + keypair.publicKey);
+console.log("Public key for USER A: " + keypair.publicKey.toBase58());
+
+const keyPair = getKeypairFromEnvironment("SECRET_KEY_2");
+console.log("Public key for USER B: ", keyPair.publicKey.toBase58());
 
 console.log("Finished");
