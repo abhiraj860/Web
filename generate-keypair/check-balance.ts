bfdb380063b087922a0fcebf1e2ec16b990a7610
Abhiraj Aditya <abhiaditya860@gmail.com>
2024-02-24 12:05:01 +0530

    two-accnts-added-in-check-balance

--- generate-keypair/check-balance.ts
+++ generate-keypair/check-balance.ts
@@ -1,7 +1,17 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 import { Connection, LAMPORTS_PER_SOL, PublicKey, clusterApiUrl } from "@solana/web3.js";
+import { getKeypairFromEnvironment } from '@solana-developers/helpers';
+
+const keyPair1 = getKeypairFromEnvironment("SECRET_KEY");
+const publicKey1 = keyPair1.publicKey;
+
+const keyPair2 = getKeypairFromEnvironment("SECRET_KEY_2");
+const publicKey2 = keyPair2.publicKey;
 
 try {
-    const publicKey = new PublicKey("BMZCLCod4aKohTSHsNw4hdEZ6vjyJbzMRsyY7nHRxiDe");
+    const publicKey = publicKey1;
     if(PublicKey.isOnCurve(publicKey)) {
         const connection = new Connection(clusterApiUrl('devnet'), 'confirmed');
 
