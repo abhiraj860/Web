cb6d7766d7e1204ee501b2705ed036b91c7f3cc3
Abhiraj Aditya <abhiaditya860@gmail.com>
2024-10-02 22:11:13 +0530

    add 2d

--- 3-app/main.go
+++ 3-app/main.go
@@ -5,9 +5,13 @@ import (
 )
 
 func main() {
-	two := [2][3]int {
-		{1, 2, 3},
-		{1, 2, 3},
+	twoD := make([][]int, 3);
+	for i := 0; i < 3; i++ {
+		innerLen := i + 1
+		twoD[i] = make([]int, innerLen)
+		for j := 0; j < innerLen; j++ {
+			twoD[i][j] = i + j
+		}
 	}
-	fmt.Println("2d:", two)
+	fmt.Println("2d", twoD)
 }
