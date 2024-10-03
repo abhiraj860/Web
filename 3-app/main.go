98a4145c1e9bcaba0fd79f6b8d17770092f63263
Abhiraj Aditya <abhiaditya860@gmail.com>
2024-10-03 22:53:18 +0530

    addmain

--- 3-app/main.go
+++ 3-app/main.go
@@ -6,4 +6,10 @@ func main() {
 	userId := 42
 	fmt.Println(userId)
 	fmt.Println(&userId)
+	anotherUserId := &userId
+	fmt.Println(anotherUserId)
+
+	userId = 100
+	fmt.Println(anotherUserId)
+	fmt.Println(*anotherUserId)
 }
