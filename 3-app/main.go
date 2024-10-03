6445023182566ef2699b6661da7d02fafd28da3b
Abhiraj Aditya <abhiaditya860@gmail.com>
2024-10-03 22:49:00 +0530

    main

--- 3-app/main.go
+++ 3-app/main.go
@@ -2,24 +2,8 @@ package main
 
 import "fmt"
 
-func zeroval(ival int) {
-	ival = 0
-}
-
-func zeroptr(iptr *int) {
-	*iptr = 0
-}
-
 func main() {
-
-	i := 1
-	fmt.Println("initial:", i)
-
-	zeroval(i)
-	fmt.Println("zeroval:", i)
-	
-	zeroptr(&i)
-	fmt.Println("zeroptr:", i)
-
-	fmt.Println("pointer:", &i)
+	userId := 42
+	fmt.Println(userId)
+	fmt.Println(&userId)
 }
