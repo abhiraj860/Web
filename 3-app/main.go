5abcb9248535d58fb199fc43e6e1b6a714fe579c
Abhiraj Aditya <abhiaditya860@gmail.com>
2024-10-03 19:55:35 +0530

    rec

--- 3-app/main.go
+++ 3-app/main.go
@@ -2,21 +2,14 @@ package main
 
 import "fmt"
 
-func intSeq() func() int {
-	i := 0
-	return func() int {
-		i++
-		return i
+func fact(n int) int{
+	if n == 0 {
+		return 1
 	}
+	return n * fact(n - 1);
 }
 
 func main() {
-	nextInt := intSeq()
+	fmt.Println(fact(7))	
 
-	fmt.Println(nextInt())
-	fmt.Println(nextInt())
-	fmt.Println(nextInt())
-
-	nextInts := intSeq()
-	fmt.Println(nextInts())
 }
