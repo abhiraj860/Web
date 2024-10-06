9491adea7aec7995889a99adf9a2c43736faf4b2
Abhiraj Aditya <abhiaditya860@gmail.com>
2024-10-06 11:24:26 +0530

    add hw

---
+++ 11-app/go.mod
@@ -0,0 +1,5 @@
+module app
+
+go 1.22.3
+
+require golang.org/x/net v0.30.0 // indirect
--- 11-app/main.go
+++ 11-app/main.go
@@ -1,21 +1,51 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"os"
+	"strings"
+	"golang.org/x/net/html"
 )
 
+var raw = `
+<!DOCTYPE html>
+<html>
+<body>
+	<h1>My First Heading</h1>
+	<p>My first paragraph.</p>
+	<p>HTML <a href="https://www.w3schools.com/html/html_images.asp">images</a> are defined with the img tag:</p>
+	<img src="xxx.jpg" width="104" height="142">
+</body>
+</html>`
 
+func visit(n * html.Node, pwords, ppics *int) {
+	
+	if n.Type == html.TextNode {
+		*pwords += len(strings.Fields(n.Data))
+	} else if n.Type == html.ElementNode && n.Data == "img" {
+		*ppics++	
+	}
+
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		visit(c, pwords, ppics)
+	}	
+}
+
+func countWordsAndImages(doc *html.Node) (int, int) {
+	var words, pics int
+
+	visit(doc, &words, &pics)
+
+	return words, pics
+}
 
 func main() {
-	a := [3]int{1, 2, 3}
-	b := a[0:1]
-	c := b[0:2]
-
-	fmt.Printf("a[%p] = %v \n", &a, a)
-	fmt.Printf("b[%p] = %[1]v \n", b)
-	fmt.Printf("c[%p] = %[1]v \n", 	c)
-
-	c = append(c, 5)
-	fmt.Printf("a[%p] = %v \n", &a, a)
-	fmt.Printf("c[%p] = %[1]v \n", 	c)
+	doc, err := html.Parse(bytes.NewReader([]byte(raw)))
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "parse failed: %s\n", err)
+		os.Exit(-1)
+	}
+	words, pics := countWordsAndImages(doc)
+	fmt.Printf("%d words and %d images\n", words, pics)
 }
